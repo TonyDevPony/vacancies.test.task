8a35d61d691dbe3c5ca737eeb1c86c02c72acff1
KirillDemchenko <kirildemchenko07@gmail.com>
2020-06-05 22:14:56 +0300

    **

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(Request $request, AuthenticationUtils $utils)
+    {
+        $error = $utils->getLastAuthenticationError();
+
+        $lastUsername = $utils->getLastUsername();
+        $params = $request->request->all();
+
+        return $this->render('security/login.html.twig', [
+            'error'         => $error,
+            'last_username' => $lastUsername,
+            'params'        => $params,
+        ]);
+    }
+
+    /**
+    * @Route("/logout", name="logout")
+    */
+    public function logout()
+    {
+
+    }
+}
