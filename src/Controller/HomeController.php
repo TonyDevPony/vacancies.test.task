959b054cbbf9ea5b5cd5eb5454b2c3d172bda4d3
KirillDemchenko <kirildemchenko07@gmail.com>
2020-06-06 21:46:22 +0300

    **

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/home", name="home")
      */
-    public function index()
+    public function index(AuthenticationUtils $utils)
     {
+        $user = $this->getUser();
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
