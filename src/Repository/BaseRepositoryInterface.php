5cfe865a39a0fe94e44f7fe8e2a244f789870822
KirillDemchenko <kirildemchenko07@gmail.com>
2020-06-13 00:09:31 +0300

    create BaseRepositoryInterface

---
+++ src/Repository/BaseRepositoryInterface.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Repository;
+
+/**
+ * Interface BaseRepositoryInterface
+ * @package App\Repository
+ */
+interface BaseRepositoryInterface
+{
+  /**
+   * @param array $criteria
+   * @param array|null $orderBy
+   * @param null $offset
+   * @param int $limit
+   * @return array
+   */
+  public function all(array $criteria = [], array $orderBy = null, $offset = null, $limit = 10): array;
+
+  /**
+   * @param int $id
+   * @return mixed
+   */
+  public function one(int $id);
+
+  /**
+   * @return mixed
+   */
+  public function update();
+
+  /**
+   * @param int $id
+   * @return mixed
+   */
+  public function delete(int $id);
+}
+
